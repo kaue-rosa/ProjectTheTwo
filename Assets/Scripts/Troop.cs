415407219061c8d187dd002dbcc4a480df334392
guialba <guilherme.albarrans@gmail.com>
2014-04-09 18:07:11 -0700

    Troop Find enemies

--- Assets/Scripts/Troop.cs
+++ Assets/Scripts/Troop.cs
@@ -26,23 +26,66 @@ public class Troop : MonoBehaviour {
 	public float troopSpeed = 2;
 	public TroopType type = TroopType.LEFT_TROOP;
 
+	private GameObject combatTarget;
+
+	private TroopStats unitStats;
 
 	// Use this for initialization
-	void Start () {
-		if(pm)pm.ManageTroop (this);
+	void Start () 
+	{
+		unitStats = GetComponent<TroopStats>();
+		if(!unitStats)
+			Debug.LogWarning("Stats Script not found in " + name);
+
+			if(pm)pm.ManageTroop (this);
+		StartCoroutine ("LookForEnemy");
 	}
 	
 	// Update is called once per frame
-	public Vector3 Move (Transform nextNode) {
+	public Vector3 Move (Transform nextNode) 
+	{
 		Vector3 direction = nextNode.position - transform.position;
 		transform.position += direction.normalized * Time.deltaTime * troopSpeed;
 		return direction;
 	}
 
-	public void LookForEnemy() {
+	public IEnumerator LookForEnemy() 
+	{
+		while(true)
+		{
+			
+			Collider[] targets = Physics.OverlapSphere(transform.position, unitStats.RangeOfSight); // read all enemies around
+			
+			foreach(Collider c in targets) // for each enemy found
+			{
+				Troop enemyScript = c.GetComponent<Troop>();
+
+				if(c.gameObject.layer == LayerMask.NameToLayer("Troop") && CheckIsEnemy(enemyScript.type))
+				{	
+
+					if(!combatTarget ) // make sure forget the old target
+					{
+						combatTarget = c.gameObject; // assign the target
+						Debug.Log(combatTarget);
+					}
+				}
+			}
+			yield return null; // every frame
+		}
+
 	}
 
-	public bool HasEnemyInRange() {
+	private bool CheckIsEnemy(TroopType unitType)
+	{
+		if(unitType != this.type )
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public bool HasEnemyInRange() 
+	{
 		return false;
 	}
 
--- Assets/Scripts/TroopStats.cs
+++ Assets/Scripts/TroopStats.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class TroopStats : MonoBehaviour 
 {
-	private float maxHealth = 10;
+	[SerializeField]private float maxHealth = 10;
 	public float MaxHealth
 	{
 		get
 		{ 
 			return maxHealth;
+		}
+	}
+
+	[SerializeField]private float rangeOfSight = 10;
+	public float RangeOfSight
+	{
+		get
+		{
+			return rangeOfSight;
 		}
 	}
 }
