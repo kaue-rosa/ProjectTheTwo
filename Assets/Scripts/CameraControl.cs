c2d9dcddcaa2538d6090ebb69063e4893629a8f5
guialba <guilherme.albarrans@gmail.com>
2014-04-14 15:03:25 -0700

    Camera Control

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour 
+{
+	private bool isHolding = false; 
+	private Camera myCamera;
+
+	private Vector3 initialPositionOnScreen = Vector3.zero;
+	private Vector3 currentPositionOnScreen = Vector3.zero;
+
+	[SerializeField] private float correctionFactor = 0.2f;
+
+	void Start () 
+	{
+		myCamera = Camera.main;
+	}
+
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			isHolding = true;
+			initialPositionOnScreen = Input.mousePosition;
+		}
+		if(Input.GetMouseButtonUp(0))
+			isHolding = false;
+
+		if(isHolding)
+		{
+
+			if(Input.mousePosition != currentPositionOnScreen)
+			{
+				currentPositionOnScreen = Input.mousePosition;
+
+				myCamera.transform.position += MoveCamera(initialPositionOnScreen, currentPositionOnScreen);
+
+				initialPositionOnScreen = currentPositionOnScreen;
+			}
+		}
+
+	}
+	 private Vector3 MoveCamera(Vector3 initPos, Vector3 currPos)
+	{
+		Vector3 cameraMovement = Vector3.zero;
+
+		cameraMovement = initPos - currPos;
+
+		cameraMovement *= correctionFactor;
+
+		return cameraMovement;
+	}
+}
